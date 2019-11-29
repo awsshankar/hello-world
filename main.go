6cd4bd2f69b84b470e6fa64ab960f0ef7083df4e
awsshankar <58332742+awsshankar@users.noreply.github.com>
2019-11-29 18:09:07 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"mime"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	mime.AddExtensionType(".ico", "image/x-icon")
+	mime.AddExtensionType(".svg", "image/svg+xml")
+
+	fileHandler := http.FileServer(http.Dir("/content"))
+	http.Handle("/", loggingHandler(fileHandler))
+
+	go func() {
+		log.Println("Starting up at :8080")
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}()
+
+	// Handle SIGTERM.
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGTERM)
+	log.Printf("Received signal '%v'. Exiting.", <-ch)
+}
+
+// HTTP Handler that adds logging to STDOUT
+func loggingHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.URL.Path)
+		h.ServeHTTP(w, r)
+	})
+}
